788a8de4b1d9adfb2b4e5fe6d68295ce5f2e6661
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2018-10-20 23:42:24 +0200

    creates initial footer component

---
+++ src/components/Footing.vue
@@ -0,0 +1,74 @@
+<template>
+  <footer class="footing">
+    <a class="footing-link" :href="link" :title="title">
+      <logo></logo>
+    </a>
+  </footer>
+</template>
+
+<script>
+  import Logo from '@/components/Logo';
+
+  export default {
+    data() {
+      return {
+        link: 'https://honeymustard.io',
+        title: 'honeymustard.io',
+      };
+    },
+    components: {
+      Logo,
+    },
+  };
+</script>
+
+<style lang="postcss">
+  @custom-media --grid-md-up (min-width: 768px);
+  @custom-media --grid-lg-up (min-width: 1000px);
+
+  .footing {
+    margin: 40px 0 0;
+    display: flex;
+
+    @media (--grid-md-up) {
+      margin: 80px 0 0;
+    }
+
+    @media (--grid-lg-up) {
+      margin: 120px 0 0;
+    }
+  }
+
+  .footing-link {
+    display: inline-flex;
+    flex-direction: column;
+    text-decoration: none;
+    width: 50px;
+    height: 70px;
+    background-color: #5454;
+    opacity: .95;
+    position: fixed;
+    bottom: 0;
+    transition: transform .3s ease;
+
+    &:hover,
+    &:focus {
+      transform: translateY(6px);
+    }
+
+    @media (--grid-lg-up) {
+      width: 60px;
+      height: 100px;
+    }
+
+    & .logo {
+      max-width: 60%;
+      margin: 10px auto 0;
+
+      @media (--grid-lg-up) {
+        max-width: 64%;
+        margin: 15px auto 0;
+      }
+    }
+  }
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,214 @@
+<template>
+  <svg class="logo" viewBox="0 0 140.5 150.5" x="0.5" y="0.5" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" shape-rendering="crispEdges">
+    <title>honeymustard.io</title>
+    <defs>
+      <symbol id="cube">
+        <rect width="10" height="10" class="logo-cube" />
+      </symbol>
+    </defs>
+
+    <!-- Letter: H -->
+
+    <!-- column (H,0 -->
+    <use xlink:href="#cube" x="0.5" y="0.5" />
+    <use xlink:href="#cube" x="0.5" y="10.5" />
+    <use xlink:href="#cube" x="0.5" y="20.5" />
+    <use xlink:href="#cube" x="0.5" y="20.5" />
+    <use xlink:href="#cube" x="0.5" y="120.5" />
+    <use xlink:href="#cube" x="0.5" y="130.5" />
+    <use xlink:href="#cube" x="0.5" y="140.5" />
+
+    <!-- column (H,1) -->
+    <use xlink:href="#cube" x="10.5" y="0.5" />
+    <use xlink:href="#cube" x="10.5" y="10.5" />
+    <use xlink:href="#cube" x="10.5" y="20.5" />
+    <use xlink:href="#cube" x="10.5" y="30.5" />
+    <use xlink:href="#cube" x="10.5" y="40.5" />
+    <use xlink:href="#cube" x="10.5" y="50.5" />
+    <use xlink:href="#cube" x="10.5" y="60.5" />
+    <use xlink:href="#cube" x="10.5" y="70.5" />
+    <use xlink:href="#cube" x="10.5" y="80.5" />
+    <use xlink:href="#cube" x="10.5" y="90.5" />
+    <use xlink:href="#cube" x="10.5" y="100.5" />
+    <use xlink:href="#cube" x="10.5" y="110.5" />
+    <use xlink:href="#cube" x="10.5" y="120.5" />
+    <use xlink:href="#cube" x="10.5" y="130.5" />
+    <use xlink:href="#cube" x="10.5" y="140.5" />
+
+    <!-- column (H,2) -->
+    <use xlink:href="#cube" x="20.5" y="0.5" />
+    <use xlink:href="#cube" x="20.5" y="10.5" />
+    <use xlink:href="#cube" x="20.5" y="20.5" />
+    <use xlink:href="#cube" x="20.5" y="30.5" />
+    <use xlink:href="#cube" x="20.5" y="40.5" />
+    <use xlink:href="#cube" x="20.5" y="50.5" />
+    <use xlink:href="#cube" x="20.5" y="60.5" />
+    <use xlink:href="#cube" x="20.5" y="70.5" />
+    <use xlink:href="#cube" x="20.5" y="80.5" />
+    <use xlink:href="#cube" x="20.5" y="90.5" />
+    <use xlink:href="#cube" x="20.5" y="100.5" />
+    <use xlink:href="#cube" x="20.5" y="110.5" />
+    <use xlink:href="#cube" x="20.5" y="120.5" />
+    <use xlink:href="#cube" x="20.5" y="130.5" />
+    <use xlink:href="#cube" x="20.5" y="140.5" />
+
+    <!-- column (H,3) -->
+    <use xlink:href="#cube" x="30.5" y="0.5" />
+    <use xlink:href="#cube" x="30.5" y="10.5" />
+    <use xlink:href="#cube" x="30.5" y="20.5" />
+    <use xlink:href="#cube" x="30.5" y="30.5" />
+    <use xlink:href="#cube" x="30.5" y="40.5" />
+    <use xlink:href="#cube" x="30.5" y="50.5" />
+    <use xlink:href="#cube" x="30.5" y="60.5" />
+    <use xlink:href="#cube" x="30.5" y="70.5" />
+    <use xlink:href="#cube" x="30.5" y="80.5" />
+    <use xlink:href="#cube" x="30.5" y="90.5" />
+    <use xlink:href="#cube" x="30.5" y="100.5" />
+    <use xlink:href="#cube" x="30.5" y="110.5" />
+    <use xlink:href="#cube" x="30.5" y="120.5" />
+    <use xlink:href="#cube" x="30.5" y="130.5" />
+    <use xlink:href="#cube" x="30.5" y="140.5" />
+
+    <!-- column (H,4) -->
+    <use xlink:href="#cube" x="40.5" y="0.5" />
+    <use xlink:href="#cube" x="40.5" y="10.5" />
+    <use xlink:href="#cube" x="40.5" y="20.5" />
+    <use xlink:href="#cube" x="40.5" y="30.5" />
+    <use xlink:href="#cube" x="40.5" y="40.5" />
+    <use xlink:href="#cube" x="40.5" y="50.5" />
+    <use xlink:href="#cube" x="40.5" y="60.5" />
+    <use xlink:href="#cube" x="40.5" y="70.5" />
+    <use xlink:href="#cube" x="40.5" y="80.5" />
+    <use xlink:href="#cube" x="40.5" y="90.5" />
+    <use xlink:href="#cube" x="40.5" y="100.5" />
+    <use xlink:href="#cube" x="40.5" y="110.5" />
+    <use xlink:href="#cube" x="40.5" y="120.5" />
+    <use xlink:href="#cube" x="40.5" y="130.5" />
+    <use xlink:href="#cube" x="40.5" y="140.5" />
+
+    <!-- column (H,5) -->
+    <use xlink:href="#cube" x="50.5" y="0.5" />
+    <use xlink:href="#cube" x="50.5" y="10.5" />
+    <use xlink:href="#cube" x="50.5" y="20.5" />
+    <use xlink:href="#cube" x="50.5" y="60.5" />
+    <use xlink:href="#cube" x="50.5" y="70.5" />
+    <use xlink:href="#cube" x="50.5" y="80.5" />
+    <use xlink:href="#cube" x="50.5" y="120.5" />
+    <use xlink:href="#cube" x="50.5" y="130.5" />
+    <use xlink:href="#cube" x="50.5" y="140.5" />
+
+    <!-- column (H,6) -->
+    <use xlink:href="#cube" x="60.5" y="60.5" />
+    <use xlink:href="#cube" x="60.5" y="70.5" />
+    <use xlink:href="#cube" x="60.5" y="80.5" />
+
+    <!-- column (H,7) -->
+    <use xlink:href="#cube" x="70.5" y="60.5" />
+    <use xlink:href="#cube" x="70.5" y="70.5" />
+    <use xlink:href="#cube" x="70.5" y="80.5" />
+
+    <!-- column (H,8) -->
+    <use xlink:href="#cube" x="80.5" y="0.5" />
+    <use xlink:href="#cube" x="80.5" y="10.5" />
+    <use xlink:href="#cube" x="80.5" y="20.5" />
+    <use xlink:href="#cube" x="80.5" y="60.5" />
+    <use xlink:href="#cube" x="80.5" y="70.5" />
+    <use xlink:href="#cube" x="80.5" y="80.5" />
+    <use xlink:href="#cube" x="80.5" y="120.5" />
+    <use xlink:href="#cube" x="80.5" y="130.5" />
+    <use xlink:href="#cube" x="80.5" y="140.5" />
+
+    <!-- column (H,9) -->
+    <use xlink:href="#cube" x="90.5" y="0.5" />
+    <use xlink:href="#cube" x="90.5" y="10.5" />
+    <use xlink:href="#cube" x="90.5" y="20.5" />
+    <use xlink:href="#cube" x="90.5" y="30.5" />
+    <use xlink:href="#cube" x="90.5" y="40.5" />
+    <use xlink:href="#cube" x="90.5" y="50.5" />
+    <use xlink:href="#cube" x="90.5" y="60.5" />
+    <use xlink:href="#cube" x="90.5" y="70.5" />
+    <use xlink:href="#cube" x="90.5" y="80.5" />
+    <use xlink:href="#cube" x="90.5" y="90.5" />
+    <use xlink:href="#cube" x="90.5" y="100.5" />
+    <use xlink:href="#cube" x="90.5" y="110.5" />
+    <use xlink:href="#cube" x="90.5" y="120.5" />
+    <use xlink:href="#cube" x="90.5" y="130.5" />
+    <use xlink:href="#cube" x="90.5" y="140.5" />
+
+    <!-- column (H,10) -->
+    <use xlink:href="#cube" x="100.5" y="0.5" />
+    <use xlink:href="#cube" x="100.5" y="10.5" />
+    <use xlink:href="#cube" x="100.5" y="20.5" />
+    <use xlink:href="#cube" x="100.5" y="30.5" />
+    <use xlink:href="#cube" x="100.5" y="40.5" />
+    <use xlink:href="#cube" x="100.5" y="50.5" />
+    <use xlink:href="#cube" x="100.5" y="60.5" />
+    <use xlink:href="#cube" x="100.5" y="70.5" />
+    <use xlink:href="#cube" x="100.5" y="80.5" />
+    <use xlink:href="#cube" x="100.5" y="90.5" />
+    <use xlink:href="#cube" x="100.5" y="100.5" />
+    <use xlink:href="#cube" x="100.5" y="110.5" />
+    <use xlink:href="#cube" x="100.5" y="120.5" />
+    <use xlink:href="#cube" x="100.5" y="130.5" />
+    <use xlink:href="#cube" x="100.5" y="140.5" />
+
+    <!-- column (H,11) -->
+    <use xlink:href="#cube" x="110.5" y="0.5" />
+    <use xlink:href="#cube" x="110.5" y="10.5" />
+    <use xlink:href="#cube" x="110.5" y="20.5" />
+    <use xlink:href="#cube" x="110.5" y="30.5" />
+    <use xlink:href="#cube" x="110.5" y="40.5" />
+    <use xlink:href="#cube" x="110.5" y="50.5" />
+    <use xlink:href="#cube" x="110.5" y="60.5" />
+    <use xlink:href="#cube" x="110.5" y="70.5" />
+    <use xlink:href="#cube" x="110.5" y="80.5" />
+    <use xlink:href="#cube" x="110.5" y="90.5" />
+    <use xlink:href="#cube" x="110.5" y="100.5" />
+    <use xlink:href="#cube" x="110.5" y="110.5" />
+    <use xlink:href="#cube" x="110.5" y="120.5" />
+    <use xlink:href="#cube" x="110.5" y="130.5" />
+    <use xlink:href="#cube" x="110.5" y="140.5" />
+
+    <!-- column (H,12) -->
+    <use xlink:href="#cube" x="120.5" y="0.5" />
+    <use xlink:href="#cube" x="120.5" y="10.5" />
+    <use xlink:href="#cube" x="120.5" y="20.5" />
+    <use xlink:href="#cube" x="120.5" y="30.5" />
+    <use xlink:href="#cube" x="120.5" y="40.5" />
+    <use xlink:href="#cube" x="120.5" y="50.5" />
+    <use xlink:href="#cube" x="120.5" y="60.5" />
+    <use xlink:href="#cube" x="120.5" y="70.5" />
+    <use xlink:href="#cube" x="120.5" y="80.5" />
+    <use xlink:href="#cube" x="120.5" y="90.5" />
+    <use xlink:href="#cube" x="120.5" y="100.5" />
+    <use xlink:href="#cube" x="120.5" y="110.5" />
+    <use xlink:href="#cube" x="120.5" y="120.5" />
+    <use xlink:href="#cube" x="120.5" y="130.5" />
+    <use xlink:href="#cube" x="120.5" y="140.5" />
+
+    <!-- column (H,13) -->
+    <use xlink:href="#cube" x="130.5" y="0.5" />
+    <use xlink:href="#cube" x="130.5" y="10.5" />
+    <use xlink:href="#cube" x="130.5" y="20.5" />
+    <use xlink:href="#cube" x="130.5" y="120.5" />
+    <use xlink:href="#cube" x="130.5" y="130.5" />
+    <use xlink:href="#cube" x="130.5" y="140.5" />
+  </svg>
+</template>
+
+<script>
+  export default {
+
+  };
+</script>
+
+<style lang="postcss">
+  .logo {
+    max-width: 100%;
+    display: block;
+  }
+
+  .logo-cube {
+    fill: var(--color-pink);
+  }
+</style>
